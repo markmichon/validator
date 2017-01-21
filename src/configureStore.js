09c489dcf4cc4c22fb4f5d04b95500b2b3aef322
markmichon <hello@markmichon.com>
2017-01-21 14:33:25 -0500

    update webpack and logging for production/dev

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,14 +3,18 @@ import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import rootReducer from './reducers'
 
+let middleware = [thunk]
+if (process.env.NODE_ENV !== 'production') {
+  let createLogger = require('redux-logger')
+  const logger = createLogger();
+  middleware = [...middleware, logger];
+}
 
 const configureStore = () => {
-  const logger = createLogger();
-  
-  return createStore(
-    rootReducer,
-    applyMiddleware(thunk, logger)
-  );
+    return createStore(
+      rootReducer,
+      applyMiddleware(...middleware)
+    );
 }
 
 export default configureStore;
