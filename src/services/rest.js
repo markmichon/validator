004c36a2f237d799a2e85b68d7e8ffa8034107f3
markmichon <hello@markmichon.com>
2017-08-29 21:28:29 -0400

    Fix HTTPS issue with endpoints

--- src/services/rest.js
+++ src/services/rest.js
@@ -1,16 +1,14 @@
-export const baseURL = 'http://validator-services.herokuapp.com';
+export const baseURL = "https://validator-services.herokuapp.com"
 
-export const validateHTML = (url) =>
-  fetch(baseURL + '/html?url=' + url)
-  .then((response) => response.json())
-  .then((response) => {
-    return {
-      warnings: response.messages.filter( (message) => message.type === "info"),
-      errors: response.messages.filter((message) => message.type === "error")
-    }
-  }
-  );
+export const validateHTML = url =>
+  fetch(baseURL + "/html?url=" + url)
+    .then(response => response.json())
+    .then(response => {
+      return {
+        warnings: response.messages.filter(message => message.type === "info"),
+        errors: response.messages.filter(message => message.type === "error")
+      }
+    })
 
-export const validateCSS = (url) =>
-  fetch(baseURL + '/css?url=' + url)
-  .then((response) => response.json());
+export const validateCSS = url =>
+  fetch(baseURL + "/css?url=" + url).then(response => response.json())
